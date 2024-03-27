6f6fd15b4f5592aa8176ebdc570712ab5c34ad66
Karel Tutsu <KarelOmab@gmail.com>
2024-03-27 17:03:34 -0400

    Update locustfile_crud.py

--- locustfile_crud.py
+++ locustfile_crud.py
@@ -9,16 +9,16 @@ class NogginUser(HttpUser):
 
     @task
     def get_request(self):
-        request_id = random.randint(1, 100)  # Example range, adjust based on your data
+        request_id = random.randint(1000, 10000)  # Example range, adjust based on your data
         self.client.get(f"/request/{request_id}", headers={'Authorization': self.authorization_key})
 
     @task
     def update_request(self):
-        request_id = random.randint(1, 100)
+        request_id = random.randint(10001, 20000)
         payload = {"is_handled": 1}
         self.client.put(f"/request/{request_id}", json=payload, headers={'Authorization': self.authorization_key})
 
     @task
     def delete_request(self):
-        request_id = random.randint(1, 100)
+        request_id = random.randint(25000, 65000)
         self.client.delete(f"/request/{request_id}", headers={'Authorization': self.authorization_key})
