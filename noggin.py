d581751488d5e1dc8000d3a00fa44702eacfed53
Karel Tutsu <KarelOmab@gmail.com>
2024-03-28 15:18:26 -0400

    Update noggin.py

--- noggin.py
+++ noggin.py
@@ -11,9 +11,15 @@ app = Flask(__name__)
 load_dotenv()  # Load environment variables from .env file
 
 # Configuration
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///requests.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@localhost/noggin_db'
+# Load environment variables
+DB_NAME = os.getenv('DB_NAME')
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
 
+DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -144,13 +150,9 @@ class Noggin:
     def run(self):
         with app.app_context():
             db.create_all()  # Initialize the database tables within an application context
-            
-            # Enable Write-Ahead Logging for SQLite, explicitly declaring the SQL expression as text
             db.session.commit()
             
         app.run(host=self.host, port=self.port, debug=self.debug)
-
-
 
 if __name__ == '__main__':
     noggin = Noggin()
