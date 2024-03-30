a619fa050aa2c135a2aa54f6c03f6da91354a30f
Karel Tutsu <KarelOmab@gmail.com>
2024-03-30 13:05:48 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -10,7 +10,7 @@ NOGGIN_HOST = "http://127.0.0.1"
 NOGGIN_PORT = 5000  # Adjust the URL/port as necessary
 NOGGIN_URL = f"{NOGGIN_HOST}:{NOGGIN_PORT}"  # Adjust the URL/port as necessary
 NOGGIN_DEBUG = True
-MAMMOTH_WORKERS = 2
+MAMMOTH_WORKERS = 1
 
 DB_NAME = os.getenv('DB_NAME')
 DB_HOST = os.getenv('DB_HOST')
