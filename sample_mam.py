e39c47eb45e6458470bf83321604f83e7896f66b
Karel Tutsu <KarelOmab@gmail.com>
2024-03-30 13:05:56 -0400

    Update sample_mam.py

--- sample_mam.py
+++ sample_mam.py
@@ -6,29 +6,31 @@ from constants import NOGGIN_URL, MAMMOTH_WORKERS
 
 def custom_process_function(session, request, job_id):
     # Custom logic for processing a request
-    print(f"Processing request {request.id} with job_id {job_id}")
     thread_id = threading.get_ident()
-
+    
     # Simulate some work
     import random
     for _ in range(random.randint(1, 5)):
         print(f"Request: {request.id}\tThread:{thread_id}\tDoing some heavy work...")
+        mammoth.insert_event(session, job_id, f"Request: {request.id}\tThread:{thread_id}\tDoing some heavy work...")
         time.sleep(random.randint(1, 5))
 
     return True
 
 
+
+
 # Initialize Mammoth
 
-app = Mammoth(process_request_data=custom_process_function, noggin_url=NOGGIN_URL, workers=MAMMOTH_WORKERS)
+mammoth = Mammoth(process_request_data=custom_process_function, noggin_url=NOGGIN_URL, workers=MAMMOTH_WORKERS)
 
 def signal_handler(sig, frame):
-    print("CTRL+C pressed! Stopping Mammoth application...")
-    app.stop()
+    print("CTRL+C pressed! Stopping Mammoth...")
+    mammoth.stop()
 
 if __name__ == "__main__":
     # Register SIGINT handler
     signal.signal(signal.SIGINT, signal_handler)
 
-    print("Starting Mammoth application... Press CTRL+C to stop.")
-    app.run()
+    print("Starting Mammoth... Press CTRL+C to stop.")
+    mammoth.run()
