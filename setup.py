2b563eac3066bcf15d1f0ba968dd2517fce06395
Karel Tutsu <KarelOmab@gmail.com>
2024-04-03 15:43:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='noggin',
-    version='0.1.0',
+    name='drumpler',
+    version='1.0.0',
     author='Karel Tutsu',
     author_email='karel.tutsu@gmail.com',
     description='Framework for rapidly developing a restful API that requires post processing',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourgithubusername/your_project_name',
+    url='https://github.com/KarelOmab/Drumpler',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
