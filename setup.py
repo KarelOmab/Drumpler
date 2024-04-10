f3153ed6abe8a955fc32b9341d1218ad164c8aba
Karel Tutsu <KarelOmab@gmail.com>
2024-04-10 12:48:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Drumpler',
-    version='1.1.1',
+    version='1.2.1',
     author='Karel Tutsu',
     author_email='karel.tutsu@gmail.com',
     description='Framework for rapidly developing a restful API that requires post processing',
